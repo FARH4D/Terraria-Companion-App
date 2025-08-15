4a34550e7a2d10886cc3ddf909c28817357c1847
Farhad <FarhadAhmed24@hotmail.co.uk>
2025-08-15 21:39:58 +0100

    refactor: update target api to 35

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,12 +4,12 @@ plugins {
 
 android {
     namespace = "com.farh4d.terrariacompanion"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.farh4d.terrariacompanion"
         minSdk = 24
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
